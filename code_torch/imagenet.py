d142449dd0aa8bbf402d2d4c8f45f4fad411270b
aaajeong <qkrdkwjd59@naver.com>
2023-05-07 00:35:07 +0900

    imagenet Test

--- code_torch/imagenet.py
+++ code_torch/imagenet.py
@@ -17,7 +17,9 @@ transform_3ch = transforms.Compose(
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # 3개의 채널에 대한 평균, 표준편차를 넣어준다.(정규화)(값은 최적화 값은 아님)
 
 trainset = torchvision.datasets.ImageNet('../data/imagenet', split='train', download=None, transform=transform_3ch)
+print(len(trainset))
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
 
 testset = torchvision.datasets.ImageNet('../data/imagenet', split='val', download=None, transform=transform_3ch)
+print(len(testset))
 testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False)
